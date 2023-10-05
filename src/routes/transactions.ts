6b11e9d6a3edc16512a1f6e7883e3b69889db31d
Guilherme <guiorlandin@gmail.com>
2023-10-05 13:59:45 -0300

    transaction implemented

---
+++ src/routes/transactions.ts
@@ -0,0 +1,26 @@
+import { FastifyInstance } from "fastify";
+import { knex } from "../database";
+import { z } from "zod";
+import { randomUUID } from "crypto";
+
+export async function transactionRoutes(app: FastifyInstance) {
+  app.post("/", async (request, reply) => {
+    const createTransactionBodySchema = z.object({
+      title: z.string(),
+      amount: z.number(),
+      type: z.enum(["credit", "debit"]),
+    });
+
+    const { title, amount, type } = createTransactionBodySchema.parse(
+      request.body
+    );
+
+    const transactions = await knex("transactions").insert({
+      id: randomUUID(),
+      title,
+      amount: type === "credit" ? amount : amount * -1,
+    });
+
+    return reply.status(201).send();
+  });
+}
--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,11 @@
 import fastify from "fastify";
-import { knex } from "./database";
 import { env } from "./env";
+import { transactionRoutes } from "./routes/transactions";
 
 const app = fastify();
 
-app.get("/hello", async () => {
-  const transaction = await knex("transactions").select("*");
-  return transaction;
+app.register(transactionRoutes, {
+  prefix: "transactions",
 });
 
 app
